b6b62c249a3b139c4aca7d82d4508b6ce562a471
hamsram <106877437+hamsram@users.noreply.github.com>
2026-01-11 00:00:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=moothiefroot_response.json(),use_container_width=True)
+sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
