b375ab6c406d2645b2d68d62a5092f4ba3f5eaf7
hamsram <106877437+hamsram@users.noreply.github.com>
2026-01-11 11:02:23 +0530

    Update streamlit_app.py
    challenge

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-import requests
+
 # App title
 st.title("Customize Your Smoothie 🥤")
 
@@ -12,28 +12,22 @@ st.write(
     """
 )
 
-
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 # Define the selectbox label and options (you had undefined names)
 label = "Choose a base for your smoothie"
 options = ["Banana", "Strawberry", "Mango", "Spinach", "Blueberry"]
 
-
-cnx=st.connection("snowflake")
-session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 ingredients_list=st.multiselect('cHOOSE UP TO 5 INGREDIENTS:',my_dataframe,max_selections=5)
 if ingredients_list:
     
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
-        st.subheader(fruit_chosen+' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
-        #st.text(smoothiefroot_response.json())
-        sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     st.write(ingredients_string)
     my_insert_stmt = """insert into smoothies.public.orders (ingredients, name_on_order)
 values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
@@ -46,5 +40,4 @@ values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-
 
