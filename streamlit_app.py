d06ce2bda4c1953fa284906aa8f9e7e8bd0d070b
hamsram <106877437+hamsram@users.noreply.github.com>
2026-01-11 11:03:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import pandas as pd
 
 # App title
 st.title("Customize Your Smoothie 🥤")
